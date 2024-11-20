92dca4e17710f5773a540b1a0559e589743921bc
jjgimeno <jjgimeno@gmail.com>
2024-11-20 13:29:53 +0100

    CRUDExplicacion

---
+++ CRUDExplicar/conectar.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "localhost";
+$username = "juanjo";
+$password = "1234";
+$dbname = "prueba";
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); // Habilitar el reporte de errores y excepciones
+
+try {
+    // Crear conexión
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn->set_charset("utf8mb4"); // Establecer el conjunto de caracteres
+} catch (exception $e) {
+    die("Conexión fallida: " . $e->getMessage());
+}
+?>
+
+
